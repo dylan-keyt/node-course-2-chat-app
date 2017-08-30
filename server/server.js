e2ed5b24108148f739db708322ae39076e9c8f01
Dylan Keyt <15023426+dylan-keyt@users.noreply.github.com>
2017-08-30 15:52:23 +1000

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connection');
 
+    io.emit('newMessage', {
+      from: 'admin',
+      text: 'Welcome to the chat app yeahhh wooohooo!',
+      createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+      from: 'admin',
+      text: 'A new user has joined. Please make them feel welcome.',
+      createdAt: new Date().getTime()
+    });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
