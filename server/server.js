a002adb3e721b986935a281e714e8b779ddad1b9
Dylan Keyt <15023426+dylan-keyt@users.noreply.github.com>
2017-08-31 14:23:06 +1000

    added form and messages

--- server/server.js
+++ server/server.js
@@ -14,12 +14,15 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connection');
+
   io.emit('newMessage', generateMessage('admin', 'Welcome to the chat app yeahhh wooohooo!'));
+
   socket.broadcast.emit('newMessage', generateMessage('admin', 'A new user has joined. Please make them feel welcome.'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
   });
 
   socket.on('disconnect', () => {
