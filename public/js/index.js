f5f1a6a8e0f31b644dad78df3948bc7e0425ca8e
Dylan Keyt <dkeyt@Dylans-MacBook-Pro-2.local>
2017-09-08 17:31:24 +1000

    added mustache

--- public/js/index.js
+++ public/js/index.js
@@ -9,25 +9,28 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
+  const template = jQuery('#message-template').html();
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  const li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  jQuery('#messages').append(li);
+  const html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 
 socket.on('newLocationMessage', function (message) {
+  const template = jQuery('#location-message-template').html();
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  const li = jQuery('<li></li>');
-  const a = jQuery('<a target="_blank">My current location</a>');
+  
+  const html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
-
-
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
